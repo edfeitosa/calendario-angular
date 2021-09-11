c9cd7aa462ba6581b807d5ebb7d4941395c95c33
edfeitosa <eduardofeitosadg@gmail.com>
2021-09-10 22:28:43 -0300

    criação de componente -> input

--- projects/calendario-angular/src/lib/calendario-angular.component.ts
+++ projects/calendario-angular/src/lib/calendario-angular.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'calendario-angular',
   templateUrl: './calendario-angular.component.html',
-  styleUrls: ['./calendario-angular.component.scss']
+  styleUrls: ['./styles/calendario-angular.component.scss']
 })
 export class CalendarioAngularComponent implements OnInit {
 
-  constructor() { }
+  @Input() title: string = 'Título do Input';
+  showTitle: boolean = false;
+  calendar!: FormGroup;
+
+  constructor(
+    private form: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formCalendario();
+  }
+
+  private formCalendario(): void {
+    this.calendar = this.form.group({
+      date: []
+    })
+  }
+
+  handleFocusInput(): void {
+    !this.showTitle && (this.showTitle = true);
+  }
+
+  handleFocusoutInput(): void {
+    (!this.calendar.get('date')?.value && this.showTitle) && (this.showTitle = false);
   }
 
 }
--- projects/calendario-angular/src/lib/calendario-angular.module.ts
+++ projects/calendario-angular/src/lib/calendario-angular.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { CalendarioAngularComponent } from './calendario-angular.component';
 
 @NgModule({
-  declarations: [ CalendarioAngularComponent ],
-  imports: [],
-  exports: [ CalendarioAngularComponent ]
+  declarations: [
+    CalendarioAngularComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    CalendarioAngularComponent
+  ]
 })
 export class CalendarioAngularModule { }
