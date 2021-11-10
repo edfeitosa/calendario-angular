b8542bc20a802db4103b32e129c0f5b918452bb4
edfeitosa <eduardofeitosadg@gmail.com>
2021-11-10 01:31:33 -0300

    funções com data -> data component

--- projects/calendario-angular/src/lib/components/datas/datas.component.ts
+++ projects/calendario-angular/src/lib/components/datas/datas.component.ts
@@ -10,6 +10,38 @@ export class DatasComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log('quantidade de dias -> ', this.quantidaDiasDoMes(2021, 4));
+    console.log('o primeiro dia do mês é -> ', this.primeiroDiaDoMes(2021, 4));
+  }
+
+  private formataData(ano: number, mes: number): Date {
+    return new Date(ano, mes, 0);
+  }
+
+  private quantidaDiasDoMes(ano: number, mes: number): number {
+    return this.formataData(ano, mes).getDate();
+  }
+
+  private inicioDoMes(ano: number, mes: number): number {
+    let data = this.formataData(ano, mes);
+    let primeiroDia = new Date(data.getFullYear(), data.getMonth(), 1);
+    return primeiroDia.getDay();
+  }
+
+  private primeiroDiaDoMes(ano: number, mes: number): string {
+    let ordemDia: number = this.inicioDoMes(ano, mes);
+    let diaSemana: string;
+    switch(ordemDia) {
+      case 0: diaSemana = 'domingo'; break;
+      case 1: diaSemana = 'segunda-feira'; break;
+      case 2: diaSemana = 'terça-feira'; break;
+      case 3: diaSemana = 'quarta-feira'; break;
+      case 4: diaSemana = 'quinta-feira'; break;
+      case 5: diaSemana = 'sexta-feira'; break;
+      case 6: diaSemana = 'sábado'; break;
+      default: diaSemana = 'domingo'; break;
+    }
+    return diaSemana;
   }
 
 }
